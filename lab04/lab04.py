eba1a62b7ad5402a67de4339cfd0754577991d3c
plus20909 <166559655+plus20909@users.noreply.github.com>
2024-04-09 15:11:01 -0700

    lab04

---
+++ lab04/lab04.py
@@ -0,0 +1,159 @@
+def flatten(s):
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]     # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
+    >>> x = [[1, [2, 3], [4, [5, 6]]]]
+    >>> flatten(x)
+    [1, 2, 3, 4, 5, 6]
+    >>> x
+    [[1, [2, 3], [4, [5, 6]]]]
+    >>> x = [[1, [1, [1, [1, 1, [1, 1, [1]]]], 1]]]
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, [1, [1, 1, [1, 1, [1]]]], 1]]]
+    """
+    "*** YOUR CODE HERE ***"
+    result = []
+    for x in s:
+        if isinstance (x, list):
+            result.extend (flatten(x))
+        else:
+            result.append(x)
+    return result
+
+
+def my_map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    >>> my_map(lambda x: abs(x), [1, -1, 5, 3, 0])
+    [1, 1, 5, 3, 0]
+    >>> my_map(lambda x: print(x), ['cs61a', 'summer', '2023'])
+    cs61a
+    summer
+    2023
+    [None, None, None]
+    """
+    return [fn(x) for x in seq]
+
+
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    >>> my_filter(lambda x: (x + 5) % 3 == 0, [1, 2, 3, 4, 5])
+    [1, 4]
+    >>> my_filter(lambda x: print(x), [1, 2, 3, 4, 5])
+    1
+    2
+    3
+    4
+    5
+    []
+    >>> my_filter(lambda x: max(5, x) == 5, [1, 2, 3, 4, 5, 6, 7])
+    [1, 2, 3, 4, 5]
+    """
+    return [x for x in seq if pred(x)]
+
+
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    result = seq[0]
+    for x in seq[1:]:
+        result = combiner(result,x)
+    return result
+
+
+def my_map_syntax_check():
+    """Check that your two_of_three code consists of nothing but a return statement.
+
+    >>> # You aren't expected to understand the code of this test.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(my_map)).body[0].body]
+    ['Expr', 'Return']
+    """
+    # You don't need to edit this function. It's just here to check your work.
+    return [fn(x) for x in seq]
+    my_map(lambda x: x*x, [3, 4, 5])
+
+
+
+def my_filter_syntax_check():
+    """Check that your two_of_three code consists of nothing but a return statement.
+
+    >>> # You aren't expected to understand the code of this test.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(my_filter)).body[0].body]
+    ['Expr', 'Return']
+    """
+    # You don't need to edit this function. It's just here to check your work.
+    return [x for x in seq if pred(x)]
+my_filter(lambda x: (x + 5) % 3 == 0, [3, 4, 5, 6, 7])
+
+
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> s1 = [1, 3, 5]
+    >>> s2 = [2, 4, 6]
+    >>> merge(s1, s2)
+    [1, 2, 3, 4, 5, 6]
+    >>> s1
+    [1, 3, 5]
+    >>> s2
+    [2, 4, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    >>> merge([2, 3, 4], [2, 4, 6])
+    [2, 2, 3, 4, 4, 6]
+    """
+    "*** YOUR CODE HERE ***"
+    result=[]
+    x =0
+    y =0
+    while x < len(lst1) and y < len(lst2):
+        if lst1[x] < lst2[y]:
+            result.append(lst1[x])
+            x = x+1
+        else:
+            result.append(lst2[y])
+            y = y+1
+    result.extend(lst1[x:])
+    result.extend(lst2[y:])
+    return result
+
+def count_palindromes(L):
+    """The number of palindromic words in the sequence of strings
+    L (ignoring case).
+
+    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
+    2
+    """
+    return sum(word.lower() == word.lower()[::-1] for word in L)
